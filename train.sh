a364d0fc6e8a75f560e26984b0da400a7c0b0dc6
Takuma Mori <takuma104@gmail.com>
2026-03-08 09:21:41 +0900

    run4 (for RTX5090)

--- train.sh
+++ train.sh
@@ -9,26 +9,26 @@ PROJECT_ROOT="$(cd "${SCRIPT_DIR}/../.." && pwd)"
 TRAIN_SHARDS="${SCRIPT_DIR}/datasets/train/*.tar"
 VAL_SHARDS="${SCRIPT_DIR}/datasets/val/*.tar"
 OUTPUT_DIR="${SCRIPT_DIR}/output"
-RUN_NUMBER=3
+RUN_NUMBER=4
 
 uv run accelerate launch "${SCRIPT_DIR}/src/trainer.py" \
     --train_shards "${TRAIN_SHARDS}" \
     --val_shards "${VAL_SHARDS}" \
     --output_dir "${OUTPUT_DIR}/run${RUN_NUMBER}" \
-    --batch_size 4 \
+    --batch_size 8 \
     --num_decoder_block_frozen 0 \
     --lr_g 1e-4 \
     --lr_d 1e-4 \
     --warmup_steps 100000 \
     --max_train_steps 1000000 \
-    --gradient_accumulation_steps 8 \
+    --gradient_accumulation_steps 4 \
     --max_audio_length 3.0 \
     --lambda_adv 1.0 \
     --lambda_fm 1.0 \
     --lambda_multi_res_mel 15.0 \
     --lambda_global_rms 1.0 \
-    --save_every 625 \
-    --eval_every 125 \
+    --save_every 1250 \
+    --eval_every 250 \
     --log_every 2 \
     --wandb_project Qwen3-TTS-Tokenizer-12Hz-Trainer \
     --wandb_run_name "Run${RUN_NUMBER}" \
